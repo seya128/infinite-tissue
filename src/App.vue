7d7e52f1d41e9061709b53bd6b3fdb9600d1e911
seya <shinichi.seya@gmail.com>
2020-03-04 15:46:31 +0900

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TissuePlayer msg="無限ティッシュ" />
+  </div>
+</template>
+
+<script>
+import TissuePlayer from './components/TissuePlayer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TissuePlayer
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0px;
+}
+</style>
